b1180c45a12f8fe49c1c4e9444013433c7328688
NidhitheCoder <nidhpv@gmail.com>
2020-08-30 22:57:31 +0530

    testominals carosel completed

--- app.js
+++ app.js
@@ -9,3 +9,21 @@ window.onscroll = function () {
     navBar.classList.remove('active');
   }
 };
+
+document.getElementById("leftMove").addEventListener("click", () => {
+  scrollItems("left");
+});
+
+document.getElementById("rightMove").addEventListener("click", () => {
+  scrollItems("right");
+});
+
+const scrollItems = direction => {
+  let element = document.getElementById("testominals");
+  let width = element.clientWidth;
+  if (direction === "left") {
+    element.scrollLeft -= width;
+  } else {
+    element.scrollLeft += width;
+  }
+};
