5b9b3299015f3b952ed64f713c62ef34675a9ffc
NidhitheCoder <nidhpv@gmail.com>
2020-09-01 11:30:29 +0530

    margin right added to carosal and change height of the testominal section

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const navBar = document.getElementById('main-nav');
+const navBar = document.getElementById("main-nav");
 
-window.onscroll = function () {
+window.onscroll = function() {
   var top = window.scrollY;
 
   if (top >= 100) {
-    navBar.classList.add('active');
+    navBar.classList.add("active");
   } else {
-    navBar.classList.remove('active');
+    navBar.classList.remove("active");
   }
 };
 
@@ -21,9 +21,17 @@ document.getElementById("rightMove").addEventListener("click", () => {
 const scrollItems = direction => {
   let element = document.getElementById("testominals");
   let width = element.clientWidth;
+  let widthMax = element.scrollWidth - element.clientWidth;
+
   if (direction === "left") {
     element.scrollLeft -= width;
+    if (element.style.marginRight) {
+      element.style = "margin-right:0";
+    }
   } else {
     element.scrollLeft += width;
+    if (element.scrollLeft === widthMax) {
+      element.style = "margin-right :2rem";
+    }
   }
 };
